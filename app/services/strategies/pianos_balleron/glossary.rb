c3e51e2b6a5c1eed43df82000d1da2e57f2f34d4
Arnaud Levy <contact@arnaudlevy.com>
2026-08-14 10:18:41 +0200

    wip

--- app/services/strategies/pianos_balleron/glossary.rb
+++ app/services/strategies/pianos_balleron/glossary.rb
@@ -1,9 +1,8 @@
 class Strategies::PianosBalleron::Glossary < Strategies::Base
 
   def apply_to_page
-    log @page.title
-    log @page.migration_identifier
-    log @page.html
+    log "Titre #{@page.title}"
+    log "Identifiant de migration #{@page.migration_identifier}"
   end
 
 end
