279cd39e075252ca2e47ec0f94267485f937dee6
Sébastien Gaya <sebastien.gaya@gmail.com>
2026-08-14 16:30:27 +0200

    Medias: add front

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -5,12 +5,8 @@
 # locales as you wish. All of these examples are active by default:
 ActiveSupport::Inflector.inflections(:fr) do |inflect|
   inflect.irregular "media", "medias"
-#   inflect.plural /^(ox)$/i, "\\1en"
-#   inflect.singular /^(ox)en/i, "\\1"
-#   inflect.uncountable %w( fish sheep )
 end
 
-# These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym "RESTful"
-# end
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.irregular "media", "medias"
+end
--- test/models/website/media_test.rb
+++ test/models/website/media_test.rb
@@ -1,5 +1,25 @@
 require "test_helper"
 
+# == Schema Information
+#
+# Table name: website_medias
+#
+#  id                           :uuid             not null, primary key
+#  url                          :string
+#  created_at                   :datetime         not null
+#  updated_at                   :datetime         not null
+#  osuny_active_storage_blob_id :string
+#  osuny_communication_media_id :string
+#  website_id                   :uuid             not null
+#
+# Indexes
+#
+#  index_website_medias_on_website_id  (website_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (website_id => websites.id)
+#
 class Website::MediaTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
